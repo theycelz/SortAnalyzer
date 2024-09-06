5fcc0ba02bb360c8446e70038032999c3ab25dce
Eos <100361496+theycelz@users.noreply.github.com>
2024-09-06 12:59:54 -0400

    style(helpers): alterar estilos para melhor compreensão pt1

--- helpers.c
+++ helpers.c
@@ -5,68 +5,17 @@
 
 #define MAX 10001
 
-// Merge Sort
-void merge_halves(int *v, int *temp, int ini, int meio, int fim) {
-    int i = ini, j = meio + 1, k = 0;
 
-    while (i <= meio && j <= fim) {
-        if (v[i] <= v[j]) {
-            temp[k++] = v[i++];
-        } else {
-            temp[k++] = v[j++];
-        }
-    }
-
-    while (i <= meio) temp[k++] = v[i++];
-    while (j <= fim) temp[k++] = v[j++];
-
-    for (i = 0; i < k; i++) {
-        v[ini + i] = temp[i];
-    }
-}
-
-void recursive_mergesort(int *v, int *temp, int ini, int fim) {
-    if (ini < fim) {
-        int meio = ini + (fim - ini) / 2;
-        recursive_mergesort(v, temp, ini, meio);
-        recursive_mergesort(v, temp, meio + 1, fim);
-        merge_halves(v, temp, ini, meio, fim);
-    }
-}
-
-void merge_sort_algorithm(int *v, int n) {
-    int *temp = malloc(n * sizeof(int));
-    recursive_mergesort(v, temp, 0, n - 1);
-    free(temp);
-}
-
-
-/* Insertion */
-
-void insertion_sort_algorithm(int *v, int n) {
-    int key, i, j;
-    for (j = 1; j < n; j++) {
-        key = v[j];
-        i = j - 1;
-        while (i >= 0 && v[i] > key) {
-            v[i + 1] = v[i];
-            i = i - 1;
-        }
-        v[i + 1] = key;
-    }
-}
-
-/*Bubble */
-
-void bubble_sort_algorithm(int *v, int n) {
+/* Algoritmo Bubble Sort*/
+void bubble_sort_algorithm(int *array, int lenght) {
     int swapped;
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < lenght - 1; i++) {
         swapped = 0;
-        for (int j = 0; j < n - i - 1; j++) {
-            if (v[j] > v[j + 1]) {
-                int temp = v[j];
-                v[j] = v[j + 1];
-                v[j + 1] = temp;
+        for (int j = 0; j < lenght - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                int tempValue = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tempValue;
                 swapped = 1;
             }
         }
@@ -74,152 +23,218 @@ void bubble_sort_algorithm(int *v, int n) {
     }
 }
 
-/*Heap Sort*/
-int left_child(int pai){
-    return 2*pai + 1;
-}
-
-int right_child(int pai){
-    return 2*pai + 2;
-}
-
-void fix_heap(int *v, int n, int i){
-    int maior = i;
-    int left = left_child(i);
-    int right = right_child(i);
-
-    if(left < n && v[left] > v[maior]){
-        maior = left;
-    }
-    if(right < n && v[right] > v[maior]){
-        maior = right;
-    }
-    if(maior!=i){
-        int temp = v[i];
-        v[i] = v[maior];
-        v[maior] = temp;
-        fix_heap(v, n, maior);
+/*Algoritmo Insertion Sort*/
+/*Função para execução do algoritmo Insertion Sort*/
+void insertion_sort_algorithm(int *array, int lenght) {
+    int currentValue, i, j;
+    for (j = 1; j < lenght; j++) {
+        currentValue = array[j];
+        i = j - 1;
+        while (i >= 0 && array[i] > currentValue) {
+            array[i + 1] = array[i];
+            i = i - 1;
+        }
+        array[i + 1] = currentValue;
     }
 }
 
-void initialize_max_heap(int *v, int n){
-    for(int i = n/2; i>=0; i--){
-        fix_heap(v, n, i);
+/*ALgoritmo Merge Sort*/
+
+void merge_halves(int *array, int start, int midpoint, int end){
+    int leftPartSize = midpoint - start + 1;
+    int rightPartSize = end - midpoint;
+    
+    int *leftArray = malloc(leftPartSize * sizeof(int));
+    int *rightArray = malloc(rightPartSize * sizeof(int));
+
+    for (int i = 0; i < leftPartSize; i++)
+        leftArray[i] = array[start + i];
+
+    for (int j = 0; j < rightPartSize; j++)
+        rightArray[j] = array[midpoint + 1 + j];
+
+    int i = 0, j = 0, k = start;
+
+    while (i < leftPartSize && j < rightPartSize) {
+        if (leftArray[i] <= rightArray[j])
+            array[k++] = leftArray[i++];
+        else
+            array[k++] = rightArray[j++];
+    }
+
+    while (i < leftPartSize)
+        array[k++] = leftArray[i++];
+
+    while (j < rightPartSize)
+        array[k++] = rightArray[j++];
+
+    free(leftArray);
+    free(rightArray);
+}
+
+
+void recursive_mergesort(int *array, int start, int end){
+    if(start < end){
+        int midpoint = start + (end-start)/2;
+
+        recursive_mergesort(array, start, midpoint);
+        recursive_mergesort(array, midpoint + 1, end);
+        
+        merge_halves(array, start, midpoint, end);
     }
 }
 
-void heap_sort_algorithm(int *v, int n){
-    initialize_max_heap(v, n);
+void merge_sort_algorithm(int *array, int lenght){
+    recursive_mergesort(array, 0, lenght-1);
+}
 
-    for(int i = n-1; i>0; i--){
-        int temp = v[0];
-        v[0] = v[i];
-        v[i] = temp;
-        fix_heap(v, i, 0);
+
+
+
+/*Algoritmo  Heap Sort*/
+int left_child(int parentIndex){
+    return 2*parentIndex + 1;
+}
+
+int right_child(int parentIndex){
+    return 2*parentIndex + 2;
+}
+
+
+void fix_heap(int *array, int length, int i) {
+    int largestIndex = i;
+    int leftIndex = left_child(i);
+    int rightIndex = right_child(i);
+
+    if (leftIndex < length && array[leftIndex] > array[largestIndex]) {
+        largestIndex = leftIndex;
+    }
+    if (rightIndex < length && array[rightIndex] > array[largestIndex]) {
+        largestIndex = rightIndex;
+    }
+    if (largestIndex != i) {
+        int tempValue = array[i];
+        array[i] = array[largestIndex];
+        array[largestIndex] = tempValue;
+        fix_heap(array, length, largestIndex);
+    }
+}
+
+
+void initialize_max_heap(int *array, int lenght) {
+    for (int i = lenght / 2 - 1; i >= 0; i--) {
+        fix_heap(array, lenght, i);
+    }
+}
+
+
+void heap_sort_algorithm(int *array, int lenght){
+    initialize_max_heap(array, lenght);
+
+    for(int i = lenght-1; i>0; i--){
+        int tempValue = array[0];
+        array[0] = array[i];
+        array[i] = tempValue;
+        fix_heap(array, i, 0);
     }
 }
 
 /*Quick Sort*/
-void quick_sort_recursive(int *v, int ini, int fim) {
-    if (ini >= fim) return;
-    
-    int pivot = v[ini];
-    int lt = ini, gt = fim;
-    int i = ini + 1;
-    
-    while (i <= gt) {
-        if (v[i] < pivot) {
-            int temp = v[lt];
-            v[lt] = v[i];
-            v[i] = temp;
-            lt++;
-            i++;
-        } else if (v[i] > pivot) {
-            int temp = v[i];
-            v[i] = v[gt];
-            v[gt] = temp;
-            gt--;
-        } else {
-            i++;
+int partition(int *array, int start, int end) {
+    int pivotValue = array[start];
+    int i = start, j = end;
+
+    while (i < j) {
+        while (i <= end && array[i] <= pivotValue) i++;
+        while (array[j] > pivotValue) j--;
+        if (i < j) {
+            int tempValue = array[i];
+            array[i] = array[j];
+            array[j] = tempValue;
         }
     }
-    
-    quick_sort_recursive(v, ini, lt - 1);
-    quick_sort_recursive(v, gt + 1, fim);
+
+    array[start] = array[j];
+    array[j] = pivotValue;
+    return j;
 }
 
-void quick_sort_algorithm(int *v, int n) {
-    quick_sort_recursive(v, 0, n - 1);
+void quick_sort_recursive(int *array, int start, int end) {
+    if (start < end) {
+        int p = partition(array, start, end);
+        quick_sort_recursive(array, start, p - 1);
+        quick_sort_recursive(array, p + 1, end);
+    }
+}
+
+void quick_sort_algorithm(int *array, int lenght){
+    quick_sort_recursive(array, 0, lenght-1);
 }
 
 /*Counting Sort*/
-void counting_sort_algorithm(int *v, int n) {
-    if (n <= 1) return;
-
-    int max = v[0];
-    for (int i = 1; i < n; i++) {
-        if (v[i] > max) max = v[i];
+void counting_sort_algorithm(int *array, int lenght) {
+    int highestValue = 0;
+    for (int i = 0; i < lenght; i++) {
+        if (array[i] > highestValue)
+            highestValue = array[i];
     }
 
-    int *count = calloc(max + 1, sizeof(int));
-    int *output = malloc(n * sizeof(int));
-
-    for (int i = 0; i < n; i++) {
-        count[v[i]]++;
+    int *countArray = calloc(highestValue + 1, sizeof(int));
+    if (!countArray) {
+        fprintf(stderr, "Memory allocation failed");
+        return;
     }
 
-    for (int i = 1; i <= max; i++) {
-        count[i] += count[i - 1];
+    for (int i = 0; i < lenght; i++)
+        countArray[array[i]]++;
+
+    int index = 0;
+    for (int i = 0; i <= highestValue; i++) {
+        while (countArray[i] > 0) {
+            array[index++] = i;
+            countArray[i]--;
+        }
     }
 
-    for (int i = n - 1; i >= 0; i--) {
-        output[count[v[i]] - 1] = v[i];
-        count[v[i]]--;
-    }
-
-    for (int i = 0; i < n; i++) {
-        v[i] = output[i];
-    }
-
-    free(count);
-    free(output);
+    free(countArray);
 }
 
+
 /*Retorna média*/
-double get_average(double *v, int n){
+double get_average(double *array, int lenght){
     double media = 0.0;
-    for(int i = 0; i<n; i++){
-        media = media + v[i];
+    for(int i = 0; i<lenght; i++){
+        media = media + array[i];
     }
-    return media/n;
+    return media/lenght;
 }
 
 /*Inverte vetor*/
-void flip_array(int *v, int n){
-    if(n>0){
-        int *aux = calloc(n, sizeof(int));
+void flip_array(int *array, int lenght){
+    if(lenght>0){
+        int *aux = calloc(lenght, sizeof(int));
         int iteration_count = 0;
 
-        for(int i = 0; i<n; i++){
-            aux[i] = v[i];
+        for(int i = 0; i<lenght; i++){
+            aux[i] = array[i];
         }
 
-        for(int j = n-1; j >= 0; j--){
-            v[iteration_count++] = aux[j];
+        for(int j = lenght-1; j >= 0; j--){
+            array[iteration_count++] = aux[j];
         }
     }
 }
 
 /*Embaralha vetor*/
-void randomize_array(int *v, int n){
+void randomize_array(int *array, int lenght){
     srand(time(NULL));
     int index1, index2;
-    int qtde = n * 0.1;
+    int qtde = lenght * 0.1;
     for(int i = 0; i<qtde; i++){
-        index1 = rand() % n;
-        index2 = rand() % n;
-        int temp = v[index1];
-        v[index1] = v[index2];
-        v[index2] = temp;
+        index1 = rand() % lenght;
+        index2 = rand() % lenght;
+        int tempValue = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tempValue;
     }
 }
--- helpers.h
+++ helpers.h
@@ -1,38 +1,48 @@
+// helpers.h
 #ifndef _TIMER_
 #define _TIMER_
 
 // Bubble Sort
-void bubble_sort_algorithm(int *v, int n);
+void bubble_sort_algorithm(int *array, int lenght);
 
 // Insertion Sort
-void insertion_sort_algorithm(int *v, int n);
+void insertion_sort_algorithm(int *array, int lenght);
 
 // Merge Sort
-void merge_halves(int *v, int *temp, int ini, int meio, int fim);
-void recursive_mergesort(int *v, int *temp, int ini, int fim);
-void merge_sort_algorithm(int *v, int n);
+void merge_halves(int *array, int start, int midpoint, int end); // Remover o parâmetro tempValue
+
+void recursive_mergesort(int *array, int start, int end); // Remover o parâmetro tempValue
+
+void merge_sort_algorithm(int *array, int lenght);
 
 // Heap Sort
-int left_child(int pai);
-int right_child(int pai);
-void fix_heap(int *v, int n, int i);
-void initialize_max_heap(int *v, int n);
-void heap_sort_algorithm(int *v, int n);
+int left_child(int parentIndex);
+
+int right_child(int parentIndex);
+
+void fix_heap(int *array, int lenght, int i);
+
+void initialize_max_heap(int *array, int lenght);
+
+void heap_sort_algorithm(int *array, int lenght);
 
 // Quick Sort
-void quick_sort_algorithm(int *v, int n);
-void quick_sort_recursive(int *v, int ini, int fim);
+void quick_sort_algorithm(int *array, int lenght);
+
+void quick_sort_recursive(int *array, int start, int end);
+
+int partition(int *array, int start, int end);
 
 // Counting Sort
-void counting_sort_algorithm(int *v, int n);
+void counting_sort_algorithm(int *array, int lenght);
 
 // Retorna média do tempo
-double get_average(double *v, int n);
+double get_average(double *array, int lenght);
 
 // Inverte vetor
-void flip_array(int *v, int n);
+void flip_array(int *array, int lenght);
 
 // Embaralha vetor
-void randomize_array(int *v, int n);
+void randomize_array(int *array, int lenght);
 
 #endif
